ca632ed07f7877258594050431c8d403f65d020d
JDahl <jmdahl43@gmail.com>
2022-08-15 15:12:27 -0500

    Joi, server-side validation.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const { campgroundSchema } = require('./schemas.js')
 const wrapAsync = require('./utils/wrapAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
@@ -27,6 +28,17 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body)
+
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next()
+  }
+}
+
 app.get('/', (req, res) => {
   res.render('home')
 })
@@ -44,10 +56,12 @@ app.get('/campgrounds/new', (req, res) => {
 
 app.post(
   '/campgrounds',
+  validateCampground,
   wrapAsync(async (req, res, next) => {
     // Check for data, before creating new campground
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground Data', 400)
+    // if (!req.body.campground)
+    //   throw new ExpressError('Invalid Campground Data', 400)
+
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -72,6 +86,7 @@ app.get(
 
 app.put(
   '/campgrounds/:id',
+  validateCampground,
   wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, {
